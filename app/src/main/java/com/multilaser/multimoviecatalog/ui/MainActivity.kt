850e5649f01e08f4cf7d00dd88ff4efaed31a365
Guilherme Goulart <guigagoulart@hotmail.com>
2022-05-01 20:32:35 -0300

    Created first activity view

---
+++ app/src/main/java/com/multilaser/multimoviecatalog/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.multilaser.multimoviecatalog.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.multilaser.multimoviecatalog.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
